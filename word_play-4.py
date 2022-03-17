363f95197297b5feef5d6f7339b73cc94581f29e
p22anishiyama <p22anishiyama@fairfieldprep.org>
2022-03-17 10:16:25 -0400

    revise word_play-3.py add word_play-4.py

--- word_play-4.py
+++ word_play-4.py
@@ -1,20 +1,20 @@
-# Author: ATN 3/16/22
+# Author: ATN 3/17/22
 
 wordfile = open('words.txt')
 line = wordfile.readline().strip()
 
 
 # defined the function
-def avoid(text):
+def uses_only(uses):
     counter = 0
     # checks if the line meets the requirements
     for line in wordfile:
-        if text not in line:
+        if uses in line:
             counter += 1
     return counter
             
 
-forbidden = input('Please enter the string to exclude: ')
+uses = input('Please enter the string to include: ')
 
 # calling the function within the print statement for a value
-print('{0} words found.'.format(avoid(forbidden)))
+print('{0} words found.'.format(uses_only(uses)))
