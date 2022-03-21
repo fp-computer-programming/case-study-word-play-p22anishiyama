daeef29db3f08e16ac49e4db49ea970f91e49c12
p22anishiyama <p22anishiyama@fairfieldprep.org>
2022-03-21 10:10:04 -0400

    revise word_play-5.py

--- word_play-5.py
+++ word_play-5.py
@@ -3,21 +3,18 @@
 wordfile = open('words.txt')
 line = wordfile.readline().strip()
 
+counter = 0
 
-# defined the function
-def is_abecedarian():
-    if line == line.sort() in line:
+def is_abecedarian(line):
+    if line == ''.join(sorted(line)) in line:
         return True
     else:
         return False
-
-counter = 0
-
-for line in wordfile:
-    if is_abecedarian():
-        counter += 1
         
 
+for line in wordfile:
+    if is_abecedarian(line):
+        counter += 1
 
 # calling the function within the print statement for a value
 print('{0} words found.'.format(counter))
