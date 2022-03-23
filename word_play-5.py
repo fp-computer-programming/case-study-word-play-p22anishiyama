34f59ded6b2def0e6dfa5a3273c0817018fe03ee
p22anishiyama <p22anishiyama@fairfieldprep.org>
2022-03-23 10:11:09 -0400

    add word_play-5.py

--- word_play-5.py
+++ word_play-5.py
@@ -1,20 +1,28 @@
-# Author: ATN 3/18/22
+# Author: ATN 3/23/22
 
 wordfile = open('words.txt')
 line = wordfile.readline().strip()
 
-counter = 0
 
+# created a blank list to handle the words given
+words = []
+
+
+# defined the function
 def is_abecedarian(line):
-    if line == ''.join(sorted(line)) in line:
-        return True
-    else:
-        return False
-        
-
-for line in wordfile:
-    if is_abecedarian(line):
+    counter = 0
+    while counter < len(line) - 1:
+        # checks if the line meets the requirements
+        if line[counter + 1] < line[counter]:
+            return False
         counter += 1
+    return True
 
-# calling the function within the print statement for a value
-print('{0} words found.'.format(counter))
+
+# goes through each line of the file while calling upon the function
+for line in wordfile.readline().strip():
+    if is_abecedarian(line):
+        words.append(line)
+
+# finding the amount of terms within the print statement using len()
+print('{0} words found.'.format(len(words)))
